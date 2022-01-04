cc2e512ae2086b258ab1cc8fe3907e23d0c8fcde
bx-ui <bx18812096731@163.com>
2022-01-04 12:10:12 +0800

    gHg

---
+++ react-redux/lesson3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReactReduxPage from './pages/ReactReduxPage'
+
+function App () {
+  return (
+    <div className="App">
+      <ReactReduxPage></ReactReduxPage>
+    </div>
+  );
+}
+
+export default App;
